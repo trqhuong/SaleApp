79a1d340c1a3fe75204792258d2ebe634db1f989
trqhuong <quynhhuongtran314@gmail.com>
2024-11-09 00:48:56 +0700

    init project

---
+++ saleapp/app/models.py
@@ -0,0 +1,59 @@
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, Float
+from sqlalchemy.orm import relationship
+from app import db, app
+
+class Category(db.Model):
+    id=Column(Integer,primary_key=True,autoincrement=True)
+    name = Column(String(50), unique=True)
+    products = relationship('Product', backref='category', lazy=True)
+
+class Product(db.Model):
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(50))
+    description = Column(String(255), nullable=True)
+    price = Column(Float, default=0)
+    image = Column(String(100), nullable=True)
+    active = Column(Boolean, default=True)
+    category_id = Column(Integer, ForeignKey(Category.id), nullable=False)
+
+
+if __name__ == '__main__':
+    with app.app_context():
+         #db.create_all()
+        # c1 = Category(name='Mobile')
+        # c2 = Category(name='Tablet')
+        # c3 = Category(name='Laptop')
+        #
+        # db.session.add_all([c1, c2, c3])
+        # db.session.commit()
+
+        products=[{
+        "id": 1,
+        "name": "iPhone 7 Plus",
+        "description": "Apple, 32GB, RAM: 3GB, iOS13",
+        "price": 17000000,
+        "image":
+            "https://res.cloudinary.com/dxxwcby8l/image/upload/v1647056401/ipmsmnxjydrhpo21xrd8.jpg",
+        "category_id": 1
+    }, {
+        "id": 2,
+        "name": "iPad Pro 2020",
+        "description": "Apple, 128GB, RAM: 6GB",
+        "price": 37000000,
+        "image":
+            "https://res.cloudinary.com/dxxwcby8l/image/upload/v1646729533/zuur9gzztcekmyfenkfr.jpg",
+        "category_id": 2
+    }, {
+        "id": 3,
+        "name": "Galaxy Note 10 Plus",
+        "description": "Samsung, 64GB, RAML: 6GB",
+        "price": 24000000,
+        "image":
+            "https://res.cloudinary.com/dxxwcby8l/image/upload/v1647248722/r8sjly3st7estapvj19u.jpg",
+        "category_id": 1
+    }]
+        for p in products:
+           prod = Product(**p)
+           db.session.add(prod)
+
+        db.session.commit()
